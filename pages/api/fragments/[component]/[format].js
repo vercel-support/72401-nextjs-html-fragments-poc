795110d41bd7b29ae1317c36a2c53dfe1f15d271
William Li <email@williamli.dev>
2022-03-15 17:48:46 +0800

    trying to include files in serverless function

--- pages/api/fragments/[component]/[format].js
+++ pages/api/fragments/[component]/[format].js
@@ -1,16 +1,18 @@
 import ReactDOMServer from 'react-dom/server';
 import HeaderComponent from '../../../../components/HeaderComponent'
 
-const fs = require('fs')
+const { readFile } = require('fs/promises')
+const { join } = require('path')
 
-export default function handler(req, res) {
+
+export default async function handler(req, res) {
   const { component, format } = req.query
   
   if (format === 'html') {
     const htmlFragment = ReactDOMServer.renderToStaticMarkup(<HeaderComponent>I am some inside content!</HeaderComponent>)
     res.status(200).send(htmlFragment)
   } else if (format === 'css') {
-    const styles = fs.readFileSync('./components/HeaderComponent/styles.module.css', {encoding:'utf8', flag:'r'});
+    const styles = await readFile(join(process.cwd(), '/components/HeaderComponent/styles.module.css'), 'utf8')
     res.status(200).send(styles)
   } else {
     res.status(404).send('Error: 404')
