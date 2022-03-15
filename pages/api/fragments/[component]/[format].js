fa1aeb9887901e18c9d0ca0ca363173cde335942
William Li <email@williamli.dev>
2022-03-15 17:51:21 +0800

    trying to get the file included in serverless function

--- pages/api/fragments/[component]/[format].js
+++ pages/api/fragments/[component]/[format].js
@@ -1,9 +1,10 @@
 import ReactDOMServer from 'react-dom/server';
 import HeaderComponent from '../../../../components/HeaderComponent'
 
-const { readFile } = require('fs/promises')
+const { readFileSync } = require('fs')
 const { join } = require('path')
 
+const styles = readFileSync(join(process.cwd(), '/components/HeaderComponent/styles.module.css'), 'utf8')
 
 export default async function handler(req, res) {
   const { component, format } = req.query
@@ -12,7 +13,6 @@ export default async function handler(req, res) {
     const htmlFragment = ReactDOMServer.renderToStaticMarkup(<HeaderComponent>I am some inside content!</HeaderComponent>)
     res.status(200).send(htmlFragment)
   } else if (format === 'css') {
-    const styles = await readFile(join(process.cwd(), '/components/HeaderComponent/styles.module.css'), 'utf8')
     res.status(200).send(styles)
   } else {
     res.status(404).send('Error: 404')
